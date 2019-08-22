e0be84ad6b3649cda31a6380ff02f433b0df2183
Travis Redman <travis.redman@gmail.com>
2019-08-22 15:38:56 -0700

    Add beeline Instance singleton
    An attempt to address https://github.com/honeycombio/beeline-java/issues/12
    
    uses a singleton pattern to allow initialization of the beeline with less up-front plumbing, assuming you are OK with defaults. Advanced users can still do their thing, but if you're just getting started you can just supply a dataset and a write key like so:
    
    ```
    import io.honeycomb.beeline.Instance
    
    // ...
    
    instance = Instance.getInstance(dataset, writeKey)
    
    instance.startChildSpan("root")
    instance.addField("foo", "bar")
    instance.sendActiveSpan();
    ```
    
    I am NOT a java developer so I don't know how many well-established patterns I'm violating with this approach. Let me know if I'm on the right track.

---
+++ beeline-core/src/main/java/io/honeycomb/beeline/DefaultBeeline.java
@@ -0,0 +1,143 @@
+package io.honeycomb.beeline;
+
+import java.net.URI;
+
+import io.honeycomb.beeline.tracing.Beeline;
+import io.honeycomb.beeline.tracing.Span;
+import io.honeycomb.beeline.tracing.SpanBuilderFactory;
+import io.honeycomb.beeline.tracing.SpanPostProcessor;
+import io.honeycomb.beeline.tracing.Tracer;
+import io.honeycomb.beeline.tracing.Tracing;
+import io.honeycomb.beeline.tracing.sampling.Sampling;
+import io.honeycomb.libhoney.HoneyClient;
+import io.honeycomb.libhoney.LibHoney;
+import io.honeycomb.libhoney.Options.Builder;
+
+/** 
+* DefaultBeeline is a singleton representing an initialized Beeline and its collaborator classes
+* It uses a simple set of defaults and provides a few convenience wrappers. If you need a more
+* specialized configuration, see the Beeline class and our docs at
+* https://docs.honeycomb.io/getting-data-in/java/beeline/
+*/ 
+public class DefaultBeeline {
+    private static DefaultBeeline INSTANCE;
+    private HoneyClient client;
+    private SpanPostProcessor postProcessor;
+    private SpanBuilderFactory factory;
+    private Tracer tracer;
+    private Beeline beeline;
+    private String serviceName;
+
+    private DefaultBeeline(String dataset, String serviceName, String writeKey,  URI apiHost) {
+        Builder builder = LibHoney.options().setDataset(dataset).setWriteKey(writeKey);
+        if (apiHost != null) {
+            builder.setApiHost(apiHost);
+        }
+        this.client = LibHoney.create(builder.build());
+        this.postProcessor = Tracing.createSpanProcessor(this.client, Sampling.alwaysSampler());
+        this.factory = Tracing.createSpanBuilderFactory(this.postProcessor, Sampling.alwaysSampler());
+        this.tracer = Tracing.createTracer(this.factory);
+        this.beeline = Tracing.createBeeline(this.tracer, this.factory);
+        this.serviceName = serviceName;
+    }
+
+    public synchronized static DefaultBeeline getInstance(String dataset, String serviceName, String writeKey) {
+        if (INSTANCE == null) {
+            INSTANCE = new DefaultBeeline(dataset, serviceName, writeKey, null);
+        }
+
+        return INSTANCE;
+    }
+
+    public synchronized static DefaultBeeline getInstance(String dataset, String serviceName, String writeKey, URI apiHost) {
+        if (INSTANCE == null) {
+            INSTANCE = new DefaultBeeline(dataset, serviceName, writeKey, apiHost);
+        }
+
+        return INSTANCE;
+    }
+
+    /**
+     * Starts a new span with the given span name. If no trace is active, starts a new trace, and returns
+     * the root span. To send this span, call {@link Span#close}.
+     *
+     * @param spanName will be set in the name field of this span
+     * @return a Span
+     */
+    public Span startSpan(String spanName) {
+        if (this.beeline.getActiveSpan().isNoop()) {
+            // start a new trace if no active trace
+            Span span = factory.createBuilder()
+            .setSpanName(spanName)
+            .setServiceName(serviceName)
+            .build();
+            tracer.startTrace(span);
+            
+            return span;
+        }
+        return this.beeline.startChildSpan(spanName);
+    }
+
+    /**
+     * Add a field to the active Span.
+     * <p>
+     * The field key will be prefixed with "app." to make it part of the "app" namespace for user-added fields.
+     * This is to avoid clashes and distinguish it from standard fields provided by automatic instrumentations like
+     * the "Spring Beeline".
+     *
+     * <h1>Example</h1>
+     * <pre>
+     * if (underAttack) {
+     *      beeline.addField("alert-message", "We are under attack!");
+     * }
+     * </pre>
+     *
+     * @param key   of the field.
+     * @param value of the field.
+     * @see #getActiveSpan()
+     */
+    public void addField(String key, Object value) {
+        this.beeline.addField(key, value);
+    }
+
+    /**
+     * Returns the span that is currently active on this thread - the one that is top of the stack.
+     * <p>
+     * Note, this method is null-safe - a "noop" span is returned if no trace is currently active. This may be due to
+     * the current execution not being instrumented or because the trace was not sampled.
+     *
+     * <h1>Example</h1>
+     * <pre>
+     * if (underAttack) {
+     *      final Span currentSpan = beeline.getActiveSpan()
+     *           .addField("alert-message", "We are under attack!")
+     *           .addField("alert-level", "red");
+     * }
+     * </pre>
+     *
+     * @return the active span.
+     */
+    public Span getActiveSpan() {
+        return this.beeline.getActiveSpan();
+    }
+
+    /**
+     * Sends the currently active span.
+     */
+    public void sendActiveSpan() {
+        this.beeline.getActiveSpan().close();
+    }
+
+    /** 
+     * getBeeline returns the Beeline in this instance for times when the convenience wrappers aren't sufficient
+     *
+     * @return Beeline
+     */
+    public Beeline getBeeline() {
+        return this.beeline;
+    }
+
+    public void close() {
+        this.client.close();
+    }
+}
