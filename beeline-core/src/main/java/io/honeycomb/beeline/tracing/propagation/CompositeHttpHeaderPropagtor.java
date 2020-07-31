a21ddedc01bfb754ab682807a0dc432657e83a06
Mike Goldsmth <goldsmith.mike@gmail.com>
2020-07-31 14:03:22 +0100

    create hashmap instead of using emptymap

--- beeline-core/src/main/java/io/honeycomb/beeline/tracing/propagation/CompositeHttpHeaderPropagtor.java
+++ beeline-core/src/main/java/io/honeycomb/beeline/tracing/propagation/CompositeHttpHeaderPropagtor.java
@@ -1,15 +1,13 @@
 package io.honeycomb.beeline.tracing.propagation;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
 public class CompositeHttpHeaderPropagtor implements PropagationCodec<Map<String, String>> {
 
-    // does order matter?
-    // decode should happen the order, so preferred codec is use first
-    // encode will overwrite duplicate entries (unlikely), maybe we should reverse order?
     private final List<PropagationCodec<Map<String, String>>> codecs;
 
     public CompositeHttpHeaderPropagtor(List<PropagationCodec<Map<String, String>>> codecs) {
@@ -26,7 +24,7 @@ public class CompositeHttpHeaderPropagtor implements PropagationCodec<Map<String
             PropagationContext context = codec.decode(encodedTrace);
 
             // return first context that is not empty
-            if (context.equals(PropagationContext.emptyContext())) {
+            if (context != PropagationContext.emptyContext()) {
                 return context;
             }
         }
@@ -36,12 +34,10 @@ public class CompositeHttpHeaderPropagtor implements PropagationCodec<Map<String
 
     @Override
     public Optional<Map<String, String>> encode(PropagationContext context) {
-        Map<String, String> headers = Collections.emptyMap();
+        Map<String, String> headers = new HashMap<>();
         for (PropagationCodec<Map<String, String>> codec : codecs) {
             Optional<Map<String, String>> codecheaders = codec.encode(context);
             if (codecheaders.isPresent()) {
-                // add entries from codec to headers
-                // existing entries will be overwritten
                 headers.putAll(codecheaders.get());
             }
         }
