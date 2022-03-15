16f2305637bcaad5dbe7b823e926f68fae0d32fb
Mike Goldsmth <goldsmith.mike@gmail.com>
2022-03-15 15:59:26 +0000

    add missing comment for beeline.starttrace without service name

--- beeline-core/src/main/java/io/honeycomb/beeline/tracing/Beeline.java
+++ beeline-core/src/main/java/io/honeycomb/beeline/tracing/Beeline.java
@@ -169,6 +169,29 @@ public class Beeline {
 		}
     }
 
+    /**
+     * A convenience method that starts a trace.
+     * <p>
+     * Typically this would be invoked when a service receives an external request, but before the request is
+     * processed by the service. For example in an HTTP Servlet filter before
+     * {@code javax.servlet.FilterChain#doFilter(ServletRequest, ServletResponse)}} is invoked.
+     * <p>
+     * The {@code parentContext} contains information that is needed to continue traces across process boundaries, e.g.
+     * whether the external service making the request was also traced. If the external service was also traced, the
+     * span returned by this method will:
+     * <p>
+     * - share the same trace ID <br>
+     * - be the child of the span that represents the call to this service
+     * <p>
+     * The returned span is also accessible via {@link #getActiveSpan()} because it becomes the current span.
+     *
+     * @param spanName the name of the span to create
+     * @param parentContext the parent context containing inter-process tracing information
+     * @return the new current span
+     * @see Tracer#startTrace(Span)
+     * @see io.honeycomb.beeline.tracing.propagation.HttpServerPropagator#startPropagation(HttpServerRequestAdapter) for
+     * an existing solution for starting (and closing) traces for incoming requests to an HTTP server.
+     */
     public Span startTrace(final String spanName, final PropagationContext parentContext) {
         return startTrace(spanName, parentContext, serviceName);
     }
