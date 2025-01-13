bc3dc97063465c02ed324be921118711df14837b
JamieDanielson <jamieedanielson@gmail.com>
2025-01-13 18:10:33 -0500

    add spring example app

---
+++ example-spring/src/main/java/io/honeycomb/beeline/Application.java
@@ -0,0 +1,13 @@
+package io.honeycomb.beeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+        System.out.println("Hello World!");
+    }
+
+}
---
+++ example-spring/src/main/java/io/honeycomb/beeline/HelloController.java
@@ -0,0 +1,13 @@
+package src.main.java.io.honeycomb.beeline;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+  @GetMapping("/")
+  public String index() {
+    return "Greetings from Spring Boot!";
+  }
+}
